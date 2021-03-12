57520b50dbc6a77aedaa56732c8df4bd8b18390a
Miguel <miguel>
2021-03-12 14:33:58 +0000

    converting http to mqtt

--- App/app.js
+++ App/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const mqtt = require('mqtt');
 
 http.createServer((request, response) => {
   const { headers, method, url } = request;
@@ -10,10 +10,22 @@ http.createServer((request, response) => {
     body.push(chunk);
   }).on('end', async () => {
     body = Buffer.concat(body).toString();
+    console.log("lightsSetting",body)
+    const lightsSetting = JSON.parse(body)
+    const brightnessPercentage = lightsSetting.brightnessPercentage
+    const brightnessValue = Math.round(1024 * brightnessPercentage / 100)
+    
 
-    //const hookInfo = JSON.parse(body)
-    console.log("data",body)
-    response.end();
+    const client  = mqtt.connect(process.env.MQTTLOCAL);
+    client.on('connect', function () {
+      console.log('entered');
+        client.publish('livingroom/wall/light', brightnessValue);
+        client.end();
+        response.status(200);
+        response.end();
+    })
+    
+    
     
   });
 }).listen(80);

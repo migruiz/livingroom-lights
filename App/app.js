67d05f4a04b6570cfab310e618f761fc84939cec
Miguel <miguel>
2021-03-28 22:14:07 +0100

    removing http dependency

--- App/app.js
+++ App/app.js
@@ -1,35 +1,15 @@
-const http = require('http');
 const mqtt = require('mqtt');
 
-http.createServer((request, response) => {
-  const { headers, method, url } = request;
-  let body = [];
-  request.on('error', (err) => {
-    console.error(err);
-  }).on('data', (chunk) => {
-    body.push(chunk);
-  }).on('end', async () => {
-    body = Buffer.concat(body).toString();
-    console.log("lightsSetting",body)
-    const lightsSetting = JSON.parse(body)
-    const brightnessPercentage = lightsSetting.brightnessPercentage
+const client  = mqtt.connect(process.env.MQTTLOCAL);
+client.on('connect', function () {
+  console.log('connected');
+  client.subscribe('livingroom/wall/light/httpbrightnessvalue');
+  client.on("message", function (topic, message) {
+    const brightnessPercentage =  parseInt(message.brightnessPercentage);
     const brightnessValue = Math.round(1024 * brightnessPercentage / 100)
-    
-
-    const client  = mqtt.connect(process.env.MQTTLOCAL);
-    client.on('connect', function () {
-      console.log('entered');
-      const valueString = brightnessValue.toString()
-      console.log(valueString);
-        client.publish('livingroom/wall/light',valueString);
-        client.end();
-        response.end();
-    })
-    
-    
-    
+    const brightnessValueString = brightnessValue.toString()
+    client.publish('livingroom/wall/light',brightnessValueString);
   });
-}).listen(80);
-
+});
 
 

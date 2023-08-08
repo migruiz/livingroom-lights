9c213e47a8ee9dc55d87a239d485c4dd8e8c1572
Miguel <mig.ruiz@gmail.com>
2023-08-08 20:58:24 +0100

    reverting to 20

--- App/app.js
+++ App/app.js
@@ -271,7 +271,7 @@ console.log(`Living Room lights current time ${DateTime.now()}`);
     },
     8:{
         sunRise: 6,
-        sunSet: 21
+        sunSet: 20
     },
     9:{
         sunRise: 6,
